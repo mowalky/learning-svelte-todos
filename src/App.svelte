917f16b8c783bd2ee053db6b57666347bf1b8d10
mowalky <dev@mowalky.com>
2021-10-06 14:04:05 -0500

    autofocus on new todo

--- src/App.svelte
+++ src/App.svelte
@@ -1,18 +1,22 @@
 <script>
   let todos = [];
+
   function addTodo() {
     todos = [...todos, ""];
   }
   function remove(index) {
     todos = [...todos.slice(0, index), ...todos.slice(index + 1)];
   }
+  function focus(el) {
+    el.focus();
+  }
 </script>
 
 <h1>Todos</h1>
 
 {#each todos as todo, index}
-  <input bind:value={todos[index]} /><button on:click={() => remove(index)}
-    >X</button
+  <input use:focus bind:value={todos[index]} /><button
+    on:click={() => remove(index)}>X</button
   >
 {/each}
 <button on:click={addTodo}>Add</button>
